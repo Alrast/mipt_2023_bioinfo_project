95e3d5d96c4e07adcdaffd1add9dde62cc43e3cb
Pol <gorshenina.pa@phystech.edu>
2023-12-14 16:20:04 +0300

    reunion

--- algo.py
+++ algo.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-def needl_wunsh(a,b, mismatch=2):
+def needl_wunsh(a,b, mismatch=2, match=0):
     """
     реализация алгоритма глобального выравнивания на основе расстояния Левенштейна, 
     на вход подаются 2 строки, 
@@ -13,13 +13,13 @@ def needl_wunsh(a,b, mismatch=2):
     for i in range(1, len(a)+1):
         for j in range(1, len(b)+1):
             if a[i-1]==b[j-1]:
-                f[j][i]=f[j-1][i-1]
+                f[j][i]=f[j-1][i-1]+match
             else:
                 f[j][i]=min(f[j-1][i-1]+mismatch, f[j][i-1]+gap,f[j-1][i]+gap)
     return f
-#проверка
-read1 = ['T', 'C', 'G', 'T']
-read2 = ['A', 'T', 'C', 'A', 'G', 'T']
-ans= needl_wunsh(read1, read2)
-for row in ans:
-    print(row)
+##проверка
+#read1 = ['T', 'C', 'G', 'T']
+#read2 = ['A', 'T', 'C', 'A', 'G', 'T']
+#ans= needl_wunsh(read1, read2)
+#for row in ans:
+    #print(row)
